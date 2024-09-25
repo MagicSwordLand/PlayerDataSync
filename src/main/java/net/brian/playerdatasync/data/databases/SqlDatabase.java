d8dbbcef6ef275378ad6cf7560a4495e243a2def
brian <brian030128@gmail.com>
2024-09-25 23:57:28 +0800

    預處理語句（PreparedStatement）中的問號（?）只能用於替換值（例如欄位值）

--- src/main/java/net/brian/playerdatasync/data/databases/SqlDatabase.java
+++ src/main/java/net/brian/playerdatasync/data/databases/SqlDatabase.java
@@ -27,12 +27,11 @@ public abstract class SqlDatabase extends Database {
     public void register(String id, Class<?> dataClass) throws Exception {
         PlayerDataSync.log("Registered table " + id);
         try (Connection connection = getConnection()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS ? (" +
+            try (PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS "+id+" (" +
                     "                uuid VARCHAR(255) NOT NULL UNIQUE," +
                     "                datajson LONGTEXT NOT NULL," +
                     "                complete TINYINT NOT NULL," +
                     "                primary key (uuid))")) {
-                preparedStatement.setString(1, id);
                 preparedStatement.executeUpdate();
             }
             classMap.put(dataClass, id);
@@ -44,10 +43,9 @@ public abstract class SqlDatabase extends Database {
     public <T> T getData(UUID uuid, Class<T> dataClass) throws Exception {
         String id = classMap.get(dataClass);
         try (Connection connection = getConnection()) {
-            final String statement = "SELECT * FROM ? WHERE uuid = ?";
+            final String statement = "SELECT * FROM "+id+" WHERE uuid = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
-                preparedStatement.setString(1, id);
-                preparedStatement.setString(2, uuid.toString());
+                preparedStatement.setString(1, uuid.toString());
                 final long start = System.currentTimeMillis();
                 while (true){
                     try (ResultSet resultSet = preparedStatement.executeQuery()) {
@@ -57,11 +55,10 @@ public abstract class SqlDatabase extends Database {
                         if (System.currentTimeMillis() - start > 10000 || resultSet.getInt("complete") == 1){
                             String dataJson = resultSet.getString("datajson");
                             if(dataJson == null) break;
-                            final String setCompleteStatement = "UPDATE ? SET complete = ? WHERE uuid = ?";
+                            final String setCompleteStatement = "UPDATE "+id+" SET complete = ? WHERE uuid = ?";
                             try (PreparedStatement preparedStatement2 = connection.prepareStatement(setCompleteStatement)){
-                                preparedStatement2.setString(1, id);
-                                preparedStatement2.setInt(2, 0);
-                                preparedStatement.setString(3,uuid.toString());
+                                preparedStatement2.setInt(1, 0);
+                                preparedStatement.setString(2,uuid.toString());
                                 preparedStatement2.executeUpdate();
                             }
                             return gson.fromJson(dataJson,dataClass);
@@ -82,20 +79,18 @@ public abstract class SqlDatabase extends Database {
     public void setData(String id,UUID uuid,Object object, boolean setComplete) throws SQLException {
         if(object == null) return;
         try (Connection connection = getConnection()){
-            final String statement = "INSERT INTO ? (uuid,datajson,complete) VALUES (?,?,0) ON DUPLICATE KEY UPDATE datajson = ?";
+            final String statement = "INSERT INTO "+id+" (uuid,datajson,complete) VALUES (?,?,0) ON DUPLICATE KEY UPDATE datajson = ?";
             String dataJson = gson.toJson(object);
             try (PreparedStatement preparedStatement = connection.prepareStatement(statement)){
-                preparedStatement.setString(1, id);
-                preparedStatement.setString(2, uuid.toString());
+                preparedStatement.setString(1, uuid.toString());
+                preparedStatement.setObject(2, dataJson);
                 preparedStatement.setObject(3, dataJson);
-                preparedStatement.setObject(4, dataJson);
                 preparedStatement.executeUpdate();
             }
             if(setComplete){
-                try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE ? SET complete = ? WHERE uuid = ?")) {
-                    preparedStatement.setString(1, id);
-                    preparedStatement.setInt(2, 1);
-                    preparedStatement.setString(3, uuid.toString());
+                try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE "+id+" SET complete = ? WHERE uuid = ?")) {
+                    preparedStatement.setInt(1, 1);
+                    preparedStatement.setString(2, uuid.toString());
                     preparedStatement.executeUpdate();
                 }
             }
